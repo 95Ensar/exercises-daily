eaadd0922ba3c9dec2214b3dfff135914c41d7af
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-18 14:37:28 +0200

    js ex-4

---
+++ ushtrimi-15.js
@@ -0,0 +1,20 @@
+/*
+	•	Declare variables with different data types:
+	•	a string (your name),
+	•	a number (your age),
+	•	a boolean (true if you like coding),
+	•	null and undefined.
+	•	Print the typeof each one.
+*/
+
+let x = true;
+let z = 'Ensar';
+const PI = 3.14
+let y = null;
+let m;
+
+console.log(typeof(x));
+console.log(typeof(z));
+console.log(typeof(PI));
+console.log(typeof(y));
+console.log(typeof(m));
