36e60911c45112566ee2a59ee5c2523631324569
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-18 14:54:52 +0200

    js ex-9

---
+++ ushtrimi-20.js
@@ -0,0 +1,24 @@
+/*
+	Write a program that checks:
+	•	if x is greater than y
+	•	if x is not equal to y
+	•	combine two conditions with && and ||
+	•	Try out ++ and -- operators on a number.
+*/
+
+let x = 14;
+let z = 20;
+
+console.log(x > z);
+console.log(x != z);
+
+console.log((x > z) && (x != z));
+console.log((x > z) || (x != z));
+
+x++
+z++
+x--
+z--
+
+console.log(x);
+console.log(z);
