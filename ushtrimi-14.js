232921dc05f6c327b74560d50c8a4311547b48ef
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-18 14:31:43 +0200

    js ex 3

---
+++ ushtrimi-14.js
@@ -0,0 +1,20 @@
+/*
+Create an expression that calculates the area of a rectangle (length * width) and prints the result.
+	•	Create another expression that calculates 5 + 3 * 2 and explain why the result is 11, not 16.
+*/
+
+
+let x = 5;
+let z = 10;
+
+let y = x * z;
+
+console.log(y);
+
+
+// ex 2
+
+let calc = 5 + 3 * 2;
+console.log(calc);
+
+//The result is 11 not 16 because the * has higher precedence than the + 
