3dfdadc359601a2bdd739a0a2348c78bd3a3180c
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-18 14:46:38 +0200

    js ex-7

---
+++ ushtrimi-18.js
@@ -0,0 +1,7 @@
+// 	•	Write an if block that checks if a number is positive or negative and prints the result.
+
+let number = 5;
+
+if(number > 0){
+    console.log('The number is greater than 0');
+}
