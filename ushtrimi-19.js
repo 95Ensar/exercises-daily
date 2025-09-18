c062f154179f3ad1459c46e758cac620fdec4a0f
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-18 14:48:26 +0200

    js ex-8

---
+++ ushtrimi-19.js
@@ -0,0 +1,16 @@
+/*
+	•	Create a variable inside a block ({ }) and try to access it outside. What happens?
+	•	Do the same with var and let to see the difference.
+
+*/
+
+{
+
+    let x = 20;
+    var y = 14;
+
+}
+
+// console.log(x);
+
+console.log(y);
