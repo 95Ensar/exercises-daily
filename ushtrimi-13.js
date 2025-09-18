f04ff2649d4e92b25bcefa8602a9e3c889415496
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-18 14:28:27 +0200

    js ex-2

---
+++ ushtrimi-13.js
@@ -0,0 +1,9 @@
+// 	•	Add a single-line comment and a multi-line comment in your code that explains what your program does.
+
+// this is a single line comment
+
+/*
+this is a 
+multi line 
+comment
+*/
