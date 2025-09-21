e4108c30db46812a2e3ff837f53f60502fa23b9c
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-21 16:08:21 +0200

    js ex-12

---
+++ ushtrimi-25.js
@@ -0,0 +1,11 @@
+      // Problem: Find the factorial of a number n.
+        let n = 3;
+        let res = 1;
+
+        for(let i = 1 ; i <= n; i++){
+                res *= i;
+             
+
+            }
+
+        console.log(res);
