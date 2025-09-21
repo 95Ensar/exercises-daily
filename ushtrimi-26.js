3deca7ca5d1ff24a82d21da142bd960157d86ecb
Ensar Sulejmani <sulejmaniensar@outlook.com>
2025-09-21 16:27:40 +0200

    js ex-13

---
+++ ushtrimi-26.js
@@ -0,0 +1,40 @@
+// Print all even numbers between 1 and 10.
+
+// for(let i = 1; i <=10 ; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+// ✅ Exercise 2: Sum of Odd Numbers
+
+// Find the sum of all odd numbers between 1 and 15.
+// let res = 0;
+// for(let i = 1 ; i <= 15; i++){
+//     if(i % 2 !== 0){
+//         res += i;
+//     }
+// }
+// console.log(res);
+
+// ✅ Exercise 3: Multiplication Table
+
+// Ask the user for a number n, and print its multiplication table up to 10.
+
+// let n = 8; 
+
+// for(let i = 1; i <= n ; i++){
+//     console.log(`${n} x ${i} = ${n * i}`);
+
+// }
+
+// Exercise 4: Reverse Countdown
+
+// Print numbers from 10 down to 1.
+
+// let i = 10; 
+// while(i >= 1){
+//     console.log(i);
+//     i--
+// }
